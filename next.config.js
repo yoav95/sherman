774f57c7b613dd4135e72f42d5e94836ed26cf69
Joav <yoav@gmail.com>
2023-08-27 13:37:57 +0300

    added 404 redirection

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "/",
+      },
+    ];
+  },
   trailingSlash: true,
   output: {
     export: true,
---
+++ src/app/not-found.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Custom404 = () => {
+  return (
+    <div>
+      <h1>הדף לא נמצא</h1>
+    </div>
+  );
+};
+
+export default Custom404;
