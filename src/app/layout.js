1d744b807cb7780327f58bebc4a118289c136628
Joav <yoav@gmail.com>
2023-08-27 12:43:53 +0300

    added vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import Navbar from "@/components/Navbar/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
-
+import { Analytics } from "@vercel/analytics/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -19,6 +19,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Navbar />
         {children}
+        <Analytics />
       </body>
     </html>
   );
