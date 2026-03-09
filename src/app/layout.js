ffe30d510f7e29bcb6dec71374e1a51c129782a9
CodeMania-100 <achzaka100@gmail.com>
2026-03-09 14:28:52 +0200

    add google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,12 +2,12 @@ import Navbar from "@/components/Navbar/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
-// import Script from "next/script";
+import Script from "next/script";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: `שרמן נדל"ן | דף הבית`,
-
   description: `שרמן נדל"ן, נדל"ן בכל חלקי הארץ. נדל"ן בצפון הארץ, נכסים להשכרה ומכירה, נכסים בקריית שמונה`,
 };
 
@@ -16,12 +16,27 @@ export default function RootLayout({ children }) {
     <html lang="he" dir="rtl">
       <head>
         <link rel="icon" href="/favicon.ico" />
+
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-DH4MM0QSMH"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DH4MM0QSMH');
+          `}
+        </Script>
+
       </head>
+
       <body className={inter.className}>
         <Navbar />
         {children}
         <Analytics />
-        {/* <Script src="nagishli.js" async /> */}
       </body>
     </html>
   );
